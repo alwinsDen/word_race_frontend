51de1fa82eb5da59e234309dc38d84bedbc55fcd
alwinsDen <jsmadesimple@gmail.com>
2022-03-02 00:40:00 +0530

    removed logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React from "react"
 import {BrowserRouter} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import Routes from "./router/Routes"
@@ -6,15 +6,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://backend-word.herokuapp.com/"
 const App = () => {
-
-    useEffect(() => {
-        let sleepLogger = setInterval(async () => {
-            let data = await fetch("https://word-race-test.herokuapp.com")
-            let serverFetch = await fetch("https://backend-word.herokuapp.com/")
-        }, 30000)
-        return () => clearInterval(sleepLogger)
-    }, [])
-
     return <BrowserRouter>
         <Routes/>
         <ToastContainer/>
