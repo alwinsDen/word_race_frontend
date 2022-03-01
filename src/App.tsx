9bd55ce089a4f218626583cefa92bc39f6c6f07a
alwinsDen <jsmadesimple@gmail.com>
2022-03-01 23:43:14 +0530

    added new website logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {BrowserRouter} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
 import Routes from "./router/Routes"
 import axios from "axios";
 
 axios.defaults.baseURL = "https://backend-word.herokuapp.com/"
-const App=()=> {
+const App = () => {
+
+    useEffect(() => {
+        let sleepLogger = setInterval(async () => {
+            let data = await fetch("https://word-race-test.herokuapp.com")
+            let serverFetch = await fetch("https://backend-word.herokuapp.com/")
+        }, 30000)
+        return () => clearInterval(sleepLogger)
+    }, [])
+
     return <BrowserRouter>
         <Routes/>
         <ToastContainer/>
